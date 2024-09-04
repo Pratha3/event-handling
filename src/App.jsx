a7f4be24ec9c03915075e47a792c6ceee6796e05
PRATHA BAGTHARIA <124965525+Pratha3@users.noreply.github.com>
2024-09-04 09:50:22 +0530

    edited committed

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ function App() {
   const [isToggled, setIsToggled] = useState(false);
   const [showCode, setShowCode] = useState(false);
   const [showHeadings, setShowHeadings] = useState(true);
+  const [color, setColor] = useState("olive");
 
   const handleClick = () => {
     setIsToggled(!isToggled);
@@ -53,6 +54,36 @@ function App() {
       <button onClick={toggleCodeVisibility} className="toggle-code-btn">
         {showCode ? "Show" : "Hide"}
       </button>
+      <div
+        className="w-full h-screen duration-200"
+        style={{ backgroundColor: color }}
+      >
+        <div className="fixed flex flex-wrap justify-center bottom-10 inset-x-0 px-3">
+          <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl">
+            <button
+              onClick={() => setColor("red")}
+              className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+              style={{ backgroundColor: "red" }}
+            >
+              Red
+            </button>
+            <button
+              onClick={() => setColor("green")}
+              className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+              style={{ backgroundColor: "green" }}
+            >
+              Green
+            </button>
+            <button
+              onClick={() => setColor("blue")}
+              className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+              style={{ backgroundColor: "blue" }}
+            >
+              Blue
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
