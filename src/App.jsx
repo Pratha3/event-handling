cbd7adc904de473ee14374097ea05ad70d18f99a
PRATHA BAGTHARIA <124965525+Pratha3@users.noreply.github.com>
2024-09-04 09:33:41 +0530

    hideshow

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,13 @@ import "./App.css";
 
 function App() {
   const [isToggled, setIsToggled] = useState(false);
+  const [showCode, setShowCode] = useState(false);
+  const [showHeadings, setShowHeadings] = useState(true);
+
   const handleClick = () => {
     setIsToggled(!isToggled);
   };
+
   const handleHover = (e) => {
     e.target.style.color = "yellow";
   };
@@ -13,33 +17,42 @@ function App() {
   const handleMouseOut = (e) => {
     e.target.style.color = "blue";
   };
+  const toggleCodeVisibility = () => {
+    setShowCode(!showCode);
+    setShowHeadings(!showHeadings);
+  };
 
   return (
     <div>
       <h1 style={{ textAlign: "center" }} className="default-effect">
         Hello!!
       </h1>
-
-      <h1
-        style={{
-          textAlign: "center",
-          fontSize: isToggled ? "38px" : "24px",
-          color: isToggled ? "red" : "black",
-        }}
-        onClick={handleClick}
-        className="click-effect"
-      >
-        React
-      </h1>
-
-      <h1
-        style={{ textAlign: "center" }}
-        className="hover-effect"
-        onMouseOver={handleHover}
-        onMouseOut={handleMouseOut}
-      >
-        Application
-      </h1>
+      {showHeadings && (
+        <>
+          <h1
+            style={{
+              textAlign: "center",
+              fontSize: isToggled ? "38px" : "24px",
+              color: isToggled ? "red" : "black",
+            }}
+            onClick={handleClick}
+            className="click-effect"
+          >
+            React
+          </h1>
+          <h1
+            style={{ textAlign: "center" }}
+            className="hover-effect"
+            onMouseOver={handleHover}
+            onMouseOut={handleMouseOut}
+          >
+            Application
+          </h1>
+        </>
+      )}
+      <button onClick={toggleCodeVisibility} className="toggle-code-btn">
+        {showCode ? "Show" : "Hide"}
+      </button>
     </div>
   );
 }
